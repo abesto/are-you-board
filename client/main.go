e9fd171e68e70d6b447ffc530c7ce5b32a69e8b0
Zoltan Nagy <abesto@abesto.net>
2015-10-08 09:25:29 +0200

    Two-way type-safe communication over websockets

--- client/main.go
+++ client/main.go
@@ -1,19 +1,52 @@
 package main
 
 import (
-	"log"
 	"encoding/json"
-	
+	"log"
+
 	"github.com/gopherjs/jquery"
+	"github.com/gopherjs/websocket"
 
 	"github.com/abesto/are-you-board/shared"
 )
 
 func main() {
-	jquery.Get("/msg", nil, nil, "text").Done(func (response string) {
+	jquery.Get("/msg", nil, nil, "text").Done(func(response string) {
 		var msg shared.ChatMessage
 		json.Unmarshal([]byte(response), &msg)
 		log.Print(msg)
 		log.Print(msg.Message)
+		go wstest()
 	})
+}
+
+func wstest() {
+	c, err := websocket.Dial("ws://localhost:8080/ws/chat")
+	if err != nil {
+		handleError(err)
+	}
+
+	myMsg := shared.ChatMessageWithoutSender{Message: "client originated this message"}
+	var buf []byte
+	buf, err = json.Marshal(myMsg)
+	if err != nil {
+		handleError(err)
+	}
+
+	c.Send(string(buf))
+
+	buf = make([]byte, 1024)
+	var n int
+	n, err = c.Read(buf)
+	if err != nil {
+		handleError(err)
+	}
+	var msg shared.ChatMessage
+	json.Unmarshal(buf[:n], &msg)
+	log.Print(string(buf[:n]))
+	log.Print(msg)
+}
+
+func handleError(err error) {
+	log.Print(err)
 }
--- server/main.go
+++ server/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"log"
 	"net/http"
-
-	"github.com/abesto/are-you-board/shared"
+	"encoding/json"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+
+	"github.com/abesto/are-you-board/shared"
 )
 
 func main() {
@@ -26,6 +29,36 @@ func main() {
 	router.GET("/msg", func(c *gin.Context) {
 		msg := shared.ChatMessage{Message: "message", Sender: "sender", Timestamp: 30}
 		c.JSON(http.StatusOK, msg)
+	})
+
+	router.GET("/ws/chat", func(c *gin.Context) {
+		conn, err := websocket.Upgrade(c.Writer, c.Request, nil, 1024, 1024)
+		if err != nil {
+			panic(err)
+		}
+		for {
+			messageType, p, err := conn.ReadMessage()
+			if err != nil {
+				log.Print(err)
+				return
+			}
+			var in shared.ChatMessageWithoutSender
+			var out shared.ChatMessage
+			json.Unmarshal(p, &in)
+			out.Message = in.Message
+			out.Sender = "whoever"
+			out.Timestamp = 3000
+			var outp []byte
+			outp, err = json.Marshal(out)
+			if err != nil {
+				log.Print(err)
+				return
+			}
+			if err = conn.WriteMessage(messageType, outp); err != nil {
+				log.Print(err)
+				return
+			}
+		}
 	})
 
 	router.Run(":8080") // listen and serve on 0.0.0.0:8080
